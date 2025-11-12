3b9936cb63288b3e03951d67eb52b5633135f65d
Arfat Chowdhury <arfatahsan60@gmail.com>
2025-11-12 17:19:57 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+require('dotenv').config();
+
+const uri = process.env.MONGODB_URI;
+// console.log('MongoDB URI:', uri)
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) =>{
+    res.send('Nemo E-commerce Server is running');
+})
+
+app.listen(port, () =>{
+    console.log(`Nemo E-commerce Server is running on port: ${port}`);
+});
